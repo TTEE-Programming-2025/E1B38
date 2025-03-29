6fc616523f5e8d48dcabeeab3f28e4b99cfa50f7
LUN-666 <u11302238@o365.ttu.edu.tw>
2025-03-30 00:20:27 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include <stdio.h>
+
+int main() {
+    printf("YU LUN\n");
+    printf("*          WELCOME TO MY APP       *\n");
+    printf("*      Created by YU LUN        *\n");
+    printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    printf("\nPress any key to exit...");
+    getchar();
+    return 0;
+}
