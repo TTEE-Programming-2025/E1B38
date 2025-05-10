81abc3a1a14177a152dd9a858f7d6d3ee512b2da
LUN-666 <u11302238@o365.ttu.edu.tw>
2025-05-10 22:43:57 +0800

    Finished Task 2 – Main Menu

--- HW3.cpp
+++ HW3.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+void showMainMenu();
+
 int main() {
     char password[10];
     int attempts = 0;
-    
+
     printf("*********************\n");
     printf("*   WELCOME USER    *\n");
     printf("* I AM  E1B  38     *\n");
@@ -28,14 +30,13 @@ int main() {
     printf("*                   *\n");
     printf("*********************\n");
 
-  
     while (attempts < 3) {
         printf("Enter password: ");
         scanf("%s", password);
 
         if (strcmp(password, "2025") == 0) {
             printf("Password correct! Welcome!\n");
-            break; 
+            break;
         } else {
             printf("Wrong password!\n");
             attempts++;
@@ -44,8 +45,21 @@ int main() {
 
     if (attempts == 3) {
         printf("Too many attempts. Program ends.\n");
+        return 0;
     }
 
+    showMainMenu();
+
     return 0;
+}
+
+void showMainMenu() {
+    printf("\n");
+    printf("----------[Booking System]----------\n");
+    printf("|  a. Available seats              |\n");
+    printf("|  b. Arrange for you              |\n");
+    printf("|  c. Choose by yourself           |\n");
+    printf("|  d. Exit                         |\n");
+    printf("------------------------------------\n");
 }
 
