335cbe85db805f3cda6232e190ebc1331e131024
LUN-666 <u11302238@o365.ttu.edu.tw>
2025-05-10 22:20:50 +0800

    Completed the first task in personal style

---
+++ HW3.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char password[10];
+    int attempts = 0;
+    
+    printf("*********************\n");
+    printf("*   WELCOME USER    *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*  -------------    *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*   Enter Password  *\n");
+    printf("*                   *\n");
+    printf("* You have 3 tries  *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+    printf("*                   *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*                   *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*                   *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*                   *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*                   *\n");
+    printf("* I AM  E1B  38     *\n");
+    printf("*                   *\n");
+    printf("*********************\n");
+
+  
+    while (attempts < 3) {
+        printf("Enter password: ");
+        scanf("%s", password);
+
+        if (strcmp(password, "2025") == 0) {
+            printf("Password correct! Welcome!\n");
+            break; 
+        } else {
+            printf("Wrong password!\n");
+            attempts++;
+        }
+    }
+
+    if (attempts == 3) {
+        printf("Too many attempts. Program ends.\n");
+    }
+
+    return 0;
+}
+
