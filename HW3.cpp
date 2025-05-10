b8e42f45e7268b672394c6cf2b72e5c1f8964b13
LUN-666 <u11302238@o365.ttu.edu.tw>
2025-05-10 22:56:58 +0800

    Finished Task 3 – Seating Arrangement

--- HW3.cpp
+++ HW3.cpp
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define SIZE 9
+
+char seats[SIZE][SIZE];
 
 void showMainMenu();
+void generateSeats();
+void displaySeats();
 
 int main() {
     char password[10];
     int attempts = 0;
+    char choice;
+
+    srand(time(NULL));
 
     printf("*********************\n");
     printf("*   WELCOME USER    *\n");
@@ -48,7 +59,24 @@ int main() {
         return 0;
     }
 
-    showMainMenu();
+    generateSeats();
+
+    while (1) {
+        showMainMenu();
+        printf("Select an option: ");
+        scanf(" %c", &choice);
+
+        if (choice == 'a') {
+            displaySeats();
+            printf("Press any key to return to menu...\n");
+            getchar();
+            getchar();
+        } else if (choice == 'd') {
+            break;
+        } else {
+            printf("Other options not implemented yet.\n");
+        }
+    }
 
     return 0;
 }
@@ -61,5 +89,33 @@ void showMainMenu() {
     printf("|  c. Choose by yourself           |\n");
     printf("|  d. Exit                         |\n");
     printf("------------------------------------\n");
+}
+
+void generateSeats() {
+    int i, row, col, count = 0;
+    for (i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            seats[i][j] = '-';
+
+    while (count < 10) {
+        row = rand() % SIZE;
+        col = rand() % SIZE;
+        if (seats[row][col] == '-') {
+            seats[row][col] = '*';
+            count++;
+        }
+    }
+}
+
+void displaySeats() {
+    int i, j;
+    printf(" \\123456789\n");
+    for (i = SIZE - 1; i >= 0; i--) {
+        printf("%d ", i + 1);
+        for (j = 0; j < SIZE; j++) {
+            printf("%c", seats[i][j]);
+        }
+        printf("\n");
+    }
 }
 
